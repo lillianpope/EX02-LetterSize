d5c2f3789cbb00a3c4199a33119779745e601c11
profknight <ahknight@pipeline.sbcc.edu>
2019-02-01 21:12:36 -0800

    Testing

---
+++ src/main/java/edu/vcccd/vc/csv40/LetterSize.java
@@ -0,0 +1,11 @@
+package edu.vcccd.vc.csv40;
+
+public class LetterSize {
+    public static void main(String args[]) {
+        final double MMS_PER_INCH = 25.4;
+        double width = 8.5 * MMS_PER_INCH;
+        double height = 11 * MMS_PER_INCH;
+
+        System.out.printf("%.2f mm X %.2f mm\n", width, height);
+    }
+}
